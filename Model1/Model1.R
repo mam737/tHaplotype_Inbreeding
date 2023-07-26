a435e5fbbf7ff94ee8f5e92e330e82998253bfe8
Manisha Munasinghe <mam737@cornell.edu>
2023-07-26 11:04:42 -0500

    Add updated Model1 RScript

--- Model1/Model1.R
+++ Model1/Model1.R
@@ -1,9 +1,13 @@
-.libPaths('/home/brandvai/mmunasin/Rlibs4')
+# Model 1
+# 2-allele Model w/ Wild-Type + Male-Sterile t-Haplotype
+# No Reproductive Compensation + No Inbreeding
+
+.libPaths('/path/to/Rlibs4')
 library(tidyr)
 library(dplyr)
 library(rlist)
 
-source('/home/brandvai/mmunasin/LethalDistorter_Inbreeding/final_models/T_Haplotye_Functions.R')
+source('/path/to/T_Haplotye_Functions.R')
 
 # Suppress summarise info
 options(dplyr.summarise.inform = FALSE)
@@ -15,24 +19,25 @@ k <- as.numeric(args[3])
 
 epsilon <- 1/1000
 
-# Initialize starting genotype frequencies
+# Initialize Starting Genotype Frequencies
 gen <- 1
 AA_current <- 1 - epsilon
 Aa_current <- epsilon
 aa_current <- 0
 a_allele_freq_current <- 0.5*Aa_current + aa_current
 
+# Generate Empty Table for Results Each Generation
 genotype_frequencies <- matrix(0,nrow=100000,ncol=7) 
 genotype_frequencies[gen,] <- c(s,t,k,gen,AA_current,Aa_current,aa_current)
 
 a_freq_dif <- 1
 
-# See whether you can obtain an equilibrium for the distorter allele 
-# without the lethal component
+# Loop through until an equilibrium frequency for the male-sterile allele is reached
 while (gen < 100 | a_freq_dif > (1/10000)) {
-    print(gen)
-	current_mating_table <- two_allele_generate_mating_table_with_random_mating(AA_current,Aa_current,aa_current,k,s,t)
+	# Generate Mating Table from Current Genotype Frequencies
+    current_mating_table <- two_allele_generate_mating_table_with_random_mating(AA_current,Aa_current,aa_current,k,s,t)
 
+    # From Mating Table Get Genotype Frequencies In The Next Generation
 	genotype_freq_next_gen <- two_allele_get_genotype_freqs_nextgen(mating_table=current_mating_table)
 
     AA_nextgen <- as.numeric(genotype_freq_next_gen[1])
@@ -45,6 +50,7 @@ while (gen < 100 | a_freq_dif > (1/10000)) {
     a_allele_freq_nextgen <- (0.5 * Aa_nextgen) + aa_nextgen
     a_freq_dif <- abs(a_allele_freq_nextgen - a_allele_freq_current)
 
+    # Update Genotype Frequencies
     AA_current <- AA_nextgen
     Aa_current <- Aa_nextgen
     aa_current <- aa_nextgen
@@ -52,10 +58,13 @@ while (gen < 100 | a_freq_dif > (1/10000)) {
     a_allele_freq_current <- a_allele_freq_nextgen
 }
 
+# Remove Extra Generation Rows
 genotype_frequencies <- genotype_frequencies[genotype_frequencies[,4]!=0,]
+
+# Prepare Results Output File
 colnames(genotype_frequencies) <- c("s","t","k","gen","AA_freq","Aa_freq","aa_freq")
 
-filename <- paste('CO2_s=',s,'_t=',t,'_k=',k,'.txt',sep='')
+filename <- paste('Model1_s=',s,'_t=',t,'_k=',k,'.txt',sep='')
 
-output_path <- paste('/home/brandvai/mmunasin/LethalDistorter_Inbreeding/final_models/CO2_model/results/',filename,sep='')
+output_path <- paste('/path/to/Model1/results/',filename,sep='')
 write.table(genotype_frequencies,file=output_path,col.names=T,row.names=F,quote=F)
